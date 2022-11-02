0c79ec3162f9a5ebd4517c8f6957bb67d167fcc9
Takuma Kira <takuma@kirakiraworx.com>
2022-11-02 16:28:17 +0900

    React Hook FormのGet StartedのMUIを利用したサンプルを貼り付け

--- src/components/ReactHookFormMUI/index.tsx
+++ src/components/ReactHookFormMUI/index.tsx
@@ -1,14 +1,46 @@
-import Typography from '@mui/material/Typography';
+import Input from '@mui/material/Input';
+import Select from 'react-select';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 
 import Frame from '../common/Frame';
 import cls from './index.module.scss';
 
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  iceCreamType: {label: string; value: string };
+}
+
 const ReactHookFormMUI = (): JSX.Element => {
+  const { control, handleSubmit } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = data => {
+    console.log(data)
+  };
+
   return (
     <Frame title="<form> 制御: React Hook Form / UI: MUI">
-      <Typography variant="body2">
-        TODO: Form制御: React Hook Form / UI: MUI によるformを実装...
-      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="firstName"
+          control={control}
+          defaultValue=""
+          render={({ field }) => <Input {...field} />}
+        />
+        <Controller
+          name="iceCreamType"
+          control={control}
+          render={({ field }) => <Select
+            {...field}
+            options={[
+              { value: "chocolate", label: "Chocolate" },
+              { value: "strawberry", label: "Strawberry" },
+              { value: "vanilla", label: "Vanilla" }
+            ]}
+          />}
+        />
+        <input type="submit" />
+      </form>
     </Frame>
   );
 }
