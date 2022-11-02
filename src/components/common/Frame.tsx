4de5d156ad500f461ff04ed005eef78df0969c08
Takuma Kira <takuma@kirakiraworx.com>
2022-11-02 16:46:15 +0900

    見にくくなったので、レイアウトを修正

--- src/components/common/Frame.tsx
+++ src/components/common/Frame.tsx
@@ -1,12 +1,15 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import classNames from 'classnames';
+
+import cls from './Frame.module.scss';
 
 const Frame = (props: {title: string, children: React.ReactNode}): JSX.Element => {
   const {title, children} = props;
 
   return (
-    <Container>
+    <Container className={classNames(cls.container)}>
       <Typography variant="h6" component="h2">
         {title}
       </Typography>
