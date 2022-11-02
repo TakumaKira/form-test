eb67eeeafe35b956e5d5e1b1d4accc7ebb6cdaef
Takuma Kira <takuma@kirakiraworx.com>
2022-11-02 16:58:31 +0900

    Material-UIのDataGridコンポーネントのサンプルを貼り付け

--- src/components/DataGridMUI/index.tsx
+++ src/components/DataGridMUI/index.tsx
@@ -1,14 +1,53 @@
-import Typography from '@mui/material/Typography';
+import { DataGrid } from '@mui/x-data-grid';
 
 import Frame from '../common/Frame';
 import cls from './index.module.scss';
 
 const DataGridMUI = (): JSX.Element => {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params: any) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
+
   return (
     <Frame title="<data-gird> UI: MUI">
-      <Typography variant="body2">
-        TODO: UI: MUI によるdata-gridを実装...
-      </Typography>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
     </Frame>
   );
 }
