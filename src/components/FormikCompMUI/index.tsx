bb1d5a6bda7c9332664b5b8bd454d33b63b25df8
Takuma Kira <takuma@kirakiraworx.com>
2022-11-04 15:52:06 +0900

    Formikのサンプルにvalidationを追加

--- src/components/FormikCompMUI/index.tsx
+++ src/components/FormikCompMUI/index.tsx
@@ -1,27 +1,31 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, FormikProps, FieldInputProps } from 'formik';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import * as Yup from 'yup';
 
 import Frame from '../common/Frame';
 import cls from './index.module.scss';
 
+const FieldComponent = ({ field, form, ...props }: { field: FieldInputProps<string>, form: FormikProps<any>, props: React.InputHTMLAttributes<HTMLInputElement> }) =>
+  <TextField {...field} {...props} />
+
 const FormikCompMUI = (): JSX.Element => {
+  const schema = Yup.object().shape({
+    firstName: Yup.string()
+      .max(15, 'Must be 15 characters or less')
+      .required('Required'),
+    lastName: Yup.string()
+      .max(20, 'Must be 20 characters or less')
+      .required('Required'),
+    email: Yup.string().email('Invalid email address').required('Required'),
+  });
+
   return (
     <Frame title="<form> 制御: Formik / UI: Formik + MUI">
       <Formik
-        initialValues={{ email: '', password: '' }}
-        validate={values => {
-          const errors: { email?: string, password?: string } = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
+        initialValues={{ firstName: '', lastName: '', email: '' }}
+        validationSchema={schema}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
@@ -29,17 +33,24 @@ const FormikCompMUI = (): JSX.Element => {
           }, 400);
         }}
       >
-        {({ isSubmitting }) => (
-          <Form>
-            <Field type="email" name="email" component={TextField} />
-            <ErrorMessage name="email" component={Typography} />
-            <Field type="password" name="password" component={TextField} />
-            <ErrorMessage name="password" component={Typography} />
-            <Button type="submit" disabled={isSubmitting}>
-              Submit
-            </Button>
-          </Form>
-        )}
+        {({ isSubmitting, errors }) => {
+          console.log(errors);
+          return (
+            <Form>
+              <Field type="text" name="firstName" component={FieldComponent} />
+              <ErrorMessage name="firstName" component={Typography} />
+              <Field type="text" name="lastName">
+                {FieldComponent}
+              </Field>
+              <ErrorMessage name="lastName" component={Typography} />
+              <Field type="email" name="email" component={FieldComponent} />
+              <ErrorMessage name="email" component={Typography} />
+              <Button type="submit" disabled={isSubmitting}>
+                Submit
+              </Button>
+            </Form>
+          )
+        }}
       </Formik>
     </Frame>
   );
--- src/components/FormikMUI/index.tsx
+++ src/components/FormikMUI/index.tsx
@@ -1,27 +1,47 @@
-import { Formik } from 'formik';
+import { Formik, useFormik } from 'formik';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import * as Yup from 'yup';
 
 import Frame from '../common/Frame';
 import cls from './index.module.scss';
 
 const FormikMUI = (): JSX.Element => {
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+    initialValues,
+    /* and other goodies */
+  } = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, 'Must be 15 characters or less')
+        .required('Required'),
+      lastName: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+      email: Yup.string().email('Invalid email address').required('Required'),
+    }),
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <Frame title="<form> 制御: Formik / UI: MUI">
       <Formik
-        initialValues={{ email: '', password: '' }}
-        validate={values => {
-          const errors: { email?: string, password?: string } = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
+        initialValues={initialValues}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
@@ -29,46 +49,47 @@ const FormikMUI = (): JSX.Element => {
           }, 400);
         }}
       >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <TextField
-              type="email"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-            {(errors.email || touched.email || errors.email) &&
-              <Typography>
-                {errors.email && touched.email && errors.email}
-              </Typography>
-            }
-            <TextField
-              type="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-            />
-            {(errors.password || touched.password || errors.password) &&
-              <Typography>
-                {errors.password && touched.password && errors.password}
-              </Typography>
-            }
-            <Button type="submit" disabled={isSubmitting}>
-              Submit
-            </Button>
-          </form>
-        )}
+        <form onSubmit={handleSubmit}>
+          <TextField
+            type="text"
+            name="firstName"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.firstName}
+          />
+          {(errors.firstName || touched.firstName || errors.firstName) &&
+            <Typography>
+              {errors.firstName && touched.firstName && errors.firstName}
+            </Typography>
+          }
+          <TextField
+            type="text"
+            name="lastName"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.lastName}
+          />
+          {(errors.lastName || touched.lastName || errors.lastName) &&
+            <Typography>
+              {errors.lastName && touched.lastName && errors.lastName}
+            </Typography>
+          }
+          <TextField
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {(errors.email || touched.email || errors.email) &&
+            <Typography>
+              {errors.email && touched.email && errors.email}
+            </Typography>
+          }
+          <Button type="submit" disabled={isSubmitting}>
+            Submit
+          </Button>
+        </form>
       </Formik>
     </Frame>
   );
